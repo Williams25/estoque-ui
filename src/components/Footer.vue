ec2aba37b485cbc8d004b8e903738a2e0b1d5edc
Williams25 <43673479+Williams25@users.noreply.github.com>
2020-06-19 10:10:36 -0300

    texto do footer modificado

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="footer">
+    <v-footer class="mt-5" dark>
+      <v-card-text class="container">
+        <strong>Todos direitos reservados - ©</strong>
+        {{ new Date().getFullYear() }}
+      </v-card-text>
+    </v-footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({})
+};
+</script>
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.footer {
+ flex-shrink: 0;
+}
+</style>
