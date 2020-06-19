c208b983efa979f33f6dedb36fdb0531271dd310
Williams25 <43673479+Williams25@users.noreply.github.com>
2020-06-19 10:16:26 -0300

    Nova rota de produtos criada

--- src/router/index.js
+++ src/router/index.js
@@ -24,7 +24,7 @@ const routes = [
     path: '/cadastrarProduto',
     name: 'CadastroProduto',
     component: () => import('../views/CadastrarProduto')
-  }
+  },
 ]
 
 const router = new VueRouter({
