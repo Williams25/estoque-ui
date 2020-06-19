149e65e60d04fecfa7becc2e3fb8d12176248e20
Williams25 <43673479+Williams25@users.noreply.github.com>
2020-06-19 10:20:19 -0300

    Declarado novo componente

--- src/main.js
+++ src/main.js
@@ -13,11 +13,16 @@ import Login from "./components/Login"
 import CadastrarUsuario from "./components/CadastrarUsuario"
 import Produto from "./components/Produtos"
 import CadastrarProduto from "./components/CadastrarProduto"
+import Menu from './components/Menu'
+import Footer from './components/Footer'
 
 Vue.component('app-login', Login)
 Vue.component('app-cadastro-usuario', CadastrarUsuario)
+Vue.component('app-menu', Menu)
 Vue.component('app-produto', Produto)
 Vue.component('app-cadastrar-produto', CadastrarProduto)
+Vue.component('app-footer', Footer)
+
 
 Vue.config.productionTip = false
 
