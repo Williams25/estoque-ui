9304a635a498f78e5866bb5b0d6067873b5b98f9
Williams25 <43673479+Williams25@users.noreply.github.com>
2020-06-19 10:19:17 -0300

    Adicionado pacotes de icones

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import '@fortawesome/fontawesome-free/css/all.css'
-
+import '@mdi/font/css/materialdesignicons.css'
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  icons: {
+    iconfont: 'mdiSvg', // default - only for display purposes
+  },
 });
